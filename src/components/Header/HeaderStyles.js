ce142938f67587dd5afdeb448e4d7d1881f62565
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-07-07 16:35:28 +0530

    added app header

---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 15,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#167b91',
+  },
+});
+
+export default styles;
